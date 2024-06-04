dc7a332b41157029c282046f14a932144d3f13ed
Marto <53458858+mhasmat@users.noreply.github.com>
2024-06-04 14:37:37 -0300

    upload tarea

--- ejercicios_practica/1- eventos/js/script.js
+++ ejercicios_practica/1- eventos/js/script.js
@@ -15,37 +15,45 @@ Verifique que en la consola sigan apereciendo los mensajes de log
 
 const boton = document.querySelector("button");
 
-boton.addEventListener("click", () => {
-    console.log("Respuesta evento click");
+boton.onclick = () => {
+    console.log("Respuesta evento click on-event");
     boton.className = "btnClick";
-})
+}
 
-boton.addEventListener("mouseover", () => {
-    console.log("Respuesta evento mouseover");
+boton.onmouseover = () => {
+    console.log("Respuesta evento mouseover on-event");
     boton.className = "btnOver";
-})
+}
 
-boton.addEventListener("mouseout", () => {
-    console.log("Respuesta evento mouseout");
+boton.onmouseout = () => {
+    console.log("Respuesta evento mouseout on-event");
     boton.className = "btnOut";
-})
+}
 
 
 /* 2 - Enunciado
-
 Cree un evento que capture cuando cambia la caja de seleccion
 Utilice el ejemplo de clase de modo "white y black" para guiarse
 en como capturar este evento y además como capturar el valor
 actual seleccionado en la caja de seleccion.
-
 */
 
-/* 3 - Enunciado
+const select = document.querySelector('select');
 
+select.onchange = () => {
+    select.value
+    ? console.log(select.value)
+    : console.log(select.onselectionchange)
+}
+
+
+/* 3 - Enunciado
 Cree un evento que capture cuando haya finalizado de ingresas
 su nombre en el elemento "input". Deberá con un alert informar
 el nombre ingresado al finalizar su ingreso.
 PISTA: Debe utilizar el mismo tipo de evento que en el ejercicio
 anterior.
-
 */
+
+const input = document.querySelector('input');
+input.onchange = () => alert(input.value);
